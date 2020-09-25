027dcd034f9edbdfa4afa06e0f1c3d53b2d9c15c
Seth Flynn <sethaflynn@gmail.com>
2020-09-24 20:31:36 -0400

    add failsafe for reddit connection check

--- app/redcord.py
+++ app/redcord.py
@@ -1,3 +1,4 @@
+import sys
 import re
 import random
 import praw
@@ -5,12 +6,17 @@ import discord
 from discord.ext import commands
 
 # reddit api info
-reddit = praw.Reddit(client_id="REDDIT APP CLIENT ID",
-                     client_secret="REDDIT APP CLIENT SECRET",
-                     user_agent="REDDIT USER AGENT")
+reddit = praw.Reddit(client_id='CLIENT_ID',
+                     client_secret='CLIENT_SECRET',
+                     user_agent='Redcord bot for Discord')
 
-# confirm reddit
-print(reddit.read_only)
+# confirm connection to reddit
+if str(reddit.read_only) == 'True':
+    print('Connected to reddit!')
+else:
+    print('There was an issue connecting to reddit. Exiting...')
+    sys.exit()
+
 
 # prefix is .
 client = commands.Bot(command_prefix = '.')
@@ -18,19 +24,18 @@ client = commands.Bot(command_prefix = '.')
 # message for when bot is ready
 @client.event
 async def on_ready():
-    print("Redcord is ready!")
+    print('Redcord is ready!')
 
-# replace all instances of ".red" for whatever command you want to use instead
+# replace all instances of '.red' for whatever command you want to use instead
 @client.event
 async def on_message(message):
     if message.content.startswith('.red'):
         channel = message.channel
         message = message.content
         print(message)
-        post = reddit.subreddit(message.replace(".red ", "")).random().url
+        post = reddit.subreddit(message.replace('.red ', '')).random().url
         print(post)
         await channel.send(post)
 
 # enter your discord bot token here
-client.run('DISCORD BOT TOKEN')
-
+client.run('BOT_TOKEN')
